ff5d8ee9f6332542dcf953f4cc790e1e9a2164b2
Dhananjai2003 <dhananjaimurali30@gmail.com>
2024-05-09 21:40:57 +0530

    scroll to div fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+"use client"
+
 import Image from "next/image";
+import ProfileTile from "@/app/{componets}/ProfileTile"
+import Link from "next/link";
 
 export default function Home() {
+
+  const scrollToDiv = () => {
+    const element = document.getElementById('about');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+
   return (
     <div className="">
-       
+        < div onClick={scrollToDiv} className="head flex justify-center items-center w-screen h-screen">
+          <ProfileTile></ProfileTile>
+        </div>
+        <div id="about" className="h-screen">about</div>
     </div>
   );
 }
