55ad9d2d5a04f5856334dc668e7d98902ce74ed2
Dhananjai2003 <dhananjaimurali30@gmail.com>
2024-05-30 09:50:51 +0530

    .

--- app/{componets}/Footer.jsx
+++ app/{componets}/Footer.jsx
@@ -3,10 +3,6 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex justify-center align-middle w-screen bg-slate-900 h-10 mt-4'>
-      <div className="loader-bar">
-                <div className="bar"></div>
-            </div>
-
     </div>
   )
 }
